42fed54a8fd470c308ebc9b74ad179da88411811
Christian Chapman <user@debian>
2025-06-15 20:35:21 -0400

    decoder improvements

--- dec_rx.py
+++ dec_rx.py
@@ -32,9 +32,9 @@ m_corr = np.array([
         method='fft'
     )
     for idx_t in range(n_carriers)])
+m_corr = m_corr / np.std(m_corr, axis=1, keepdims=True)
 
 print('Building pulse likelihood map...')
-m_pulse_likelihood = np.full(m_corr.shape, np.nan) 
 n_lags = len(m_corr[0,:])
 
 print('Calculating pulse likelihoods...')
@@ -43,31 +43,30 @@ m_noise_var = np.array([
     for idx_lag in range(n_lags - n_pulse_len)
 ]).T
 m_query_var = np.abs(m_corr[:, n_pulse_len:(n_lags)])**2
-m_pulse_likelihood[:, :n_lags - n_pulse_len] = m_query_var - np.median(m_noise_var, axis=0)
+m_pulse_likelihood = m_query_var - np.median(m_noise_var, axis=0)
+b, a = sp.signal.butter(4, 0.45, btype='low')  
+m_pulse_likelihood = np.array([sp.signal.filtfilt(b, a, row) for row in m_pulse_likelihood])
 
 print('Building frame likelihood map')
 m_carrier_idx = np.reshape(np.arange(n_carriers), [2,n_hop_sequence_len])
 m_pulse_start_idx = n_pulse_len*np.tile(np.arange(n_hop_sequence_len),[2,1])
-v_frame_likelihood = np.full(n_lags, np.nan)
-# Vectorized computation for frame start likelihood
 m_p = np.array([
     m_pulse_likelihood[m_carrier_idx, idx_lag+m_pulse_start_idx]
     for idx_lag in range(n_lags - n_frame_len)
 ])
-v_frame_likelihood[:n_lags - n_frame_len] = np.sum(np.max(m_p, axis=2), axis=1)
+v_frame_likelihood = np.sum(np.max(m_p, axis=2), axis=1)
 
 print('Decoding frames')
-n_win_len = int(n_frame_len*1.2)
-n_advance_len = int(n_frame_len*0.4)
-v_frame_locations = np.empty(0)
+n_win_len = int(n_frame_len*1.6)
+n_advance_len = int(n_frame_len*0.2)
+v_frame_locations = np.empty(0, dtype=int)
 m_frames_soft = np.empty(0)
 idx_win = 0
-v_likelihood_win = sp.signal.windows.gaussian(n_win_len, std=0.1)
 while(idx_win < n_lags - n_frame_len):
     idx_lag = idx_win + np.argmax(v_frame_likelihood[idx_win:idx_win+n_win_len])
     v_frame_locations = np.append(v_frame_locations, idx_lag)
     m_this_frame_likelihood = m_pulse_likelihood[m_carrier_idx, idx_lag+m_pulse_start_idx]
-    v_this_frame_soft = m_this_frame_likelihood[1,:] - np.mean(m_this_frame_likelihood, axis=0)
+    v_this_frame_soft = m_this_frame_likelihood[1,:] - np.min(m_this_frame_likelihood, axis=0)
     m_frames_soft = np.vstack([m_frames_soft, v_this_frame_soft]) if m_frames_soft.size else v_this_frame_soft[np.newaxis, :]
     idx_win = idx_lag + n_advance_len
 v_bits_soft = m_frames_soft.flatten()
@@ -84,12 +83,51 @@ idx_lag = np.argmax(v_bit_correlation)
 n_err = (len(v_bits_true)-np.max(v_bit_correlation))/2
 v_bits_maxcorr = v_bits[idx_lag-len(v_bits_true)+1:idx_lag+1]
 print(f'ber: {n_err/len(v_bits_true)} ({n_err}/{len(v_bits_true)})')
-v_idx_err = np.nonzero(np.abs(v_bits_maxcorr-v_bits_true))
+v_idx_err = np.nonzero(np.abs(v_bits_maxcorr-v_bits_true[:len(v_bits_maxcorr)]))
+
+# Calculate the frames that contain the maxcorr bits
+idx_startframe = int((idx_lag-len(v_bits_true)+1) / n_hop_sequence_len)
+idx_endframe = int((idx_lag+1) / n_hop_sequence_len)
 
 if(b_plot):
+    # Plot STFT of the samples
+    # Create a subplot with the frame errors above the STFT plot
+    fig, axs = plt.subplots(2, 1, figsize=(30, 15), gridspec_kw={'height_ratios': [1, 3]})
+
+    # Frame errors plot
+    v_err = v_bits_true[:len(v_bits_maxcorr)] - v_bits_maxcorr
+    axs[0].plot(v_err, label="diff", alpha=0.7)
+    for idx in range(0, len(v_bits_true[:len(v_bits_maxcorr)]), n_hop_sequence_len):
+        axs[0].axvline(x=idx, color='gray', linestyle='--', alpha=0.5, label="Hop Sequence Boundary" if idx == 0 else None)
+    axs[0].set_title("Frame Errors")
+    axs[0].set_xlabel("Bit Index")
+    axs[0].set_ylabel("Value")
+    axs[0].legend()
+    axs[0].grid()
+    
+    # Heatmap of m_pulse_likelihood
+    m_this_frame = m_pulse_likelihood[:,v_frame_locations[idx_startframe]:v_frame_locations[idx_endframe]]
+    pcm = axs[1].imshow(
+        np.log10(np.maximum(1e-10,m_this_frame)), 
+        aspect='auto', cmap='viridis', 
+        extent=[0, m_this_frame.shape[1], 0, n_carriers], vmin=-10, vmax=0)
+    for idx_err in v_idx_err[0]:
+        axs[1].axvline(
+            x=idx_err * m_this_frame.shape[1] / len(v_err), 
+            color='red', linestyle='-', linewidth=5, alpha=0.7, label="Error Location" if idx_err == v_idx_err[0][0] else None
+        )
+    axs[1].legend()
+    axs[1].set_title("Heatmap of Pulse Likelihood Matrix (m_pulse_likelihood)")
+    axs[1].set_xlabel("Lag idx")
+    axs[1].set_ylabel("Carrier Index")
+    fig.colorbar(pcm, ax=axs[1], label="Pulse Likelihood Magnitude")
+
+    plt.tight_layout()
+    plt.show()
+
     # Histogram of error locations modulo n_hop_sequence_len
     plt.figure(figsize=(10, 6))
-    plt.hist(v_idx_err[0][23:] % n_hop_sequence_len, bins=n_hop_sequence_len, alpha=0.7, edgecolor='black')
+    plt.hist(v_idx_err[0] % n_hop_sequence_len, bins=n_hop_sequence_len, alpha=0.7, edgecolor='black')
     plt.title("Histogram of Error locations Modulo Hop Sequence Length")
     plt.xlabel("Error Index Modulo Hop Sequence Length")
     plt.ylabel("Frequency")
@@ -98,19 +136,9 @@ if(b_plot):
 
     plt.figure(figsize=(10, 6))
     plt.plot(v_frame_likelihood, label="Frame Start Likelihood")
-    for frame_location in v_frame_locations:
-        plt.axvline(x=frame_location, color='red', linestyle='--', alpha=0.7, label="Frame Location")
     plt.title("Frame Start Likelihood")
     plt.xlabel("Lag")
     plt.ylabel("Likelihood")
-    plt.legend()
-    plt.grid()
-    plt.show()
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(abs(v_bits_maxcorr-v_bits_true[:len(v_bits_maxcorr)]), label="diff", alpha=0.7)
-    plt.xlabel("Bit Index")
-    plt.ylabel("Value")
     plt.legend()
     plt.grid()
     plt.show()
--- wf.py
+++ wf.py
@@ -9,8 +9,11 @@ params = { # Waveform parameters
     "n_hop_sequence_len": 8,  # N; Length of the hopping sequence; after this many hops the hopped BFSK repeats
     "n_freq_offset": 57, # Frequency bin offset (index of a P/2-point complex DFT)
 }
+v_win_idx = 2*(np.pi*(np.arange(params["n_pulse_len_re"]) - params["n_pulse_len_re"]/2))/params["n_pulse_len_re"]
+v_pulse_win = np.sin(v_win_idx)/v_win_idx
+v_pulse_win[params["n_pulse_len_re"]//2] = 1
+v_pulse_win_fft_magnitude = np.abs(np.fft.fft(v_pulse_win))
 
-v_pulse_win = sp.signal.windows.cosine(1+params["n_pulse_len_re"])[:-1]  # Window function for real pulse shape
 def define_pulses(p=params): # Generate real pulse matrix
     n_fft_bins = p["n_pulse_len_re"] // 2
     m_pulses = np.zeros((n_fft_bins, p["n_pulse_len_re"]), dtype=np.float32) # Rows = fft bin, cols = real samples for this pulse frequency location
@@ -40,6 +43,7 @@ def make_test_wf(m_carriers=m_carriers, m_pulses=m_pulses, p=params): # Create a
     m_hops = modulate_bits(v_bits)
     v_sam = m_hops.flatten(order='C')  # Flatten the frames into a single vector
     v_sam = np.int16(v_sam / np.max(np.abs(v_sam)) * 32767)
+    v_sam = np.pad(v_sam, (params["fs"], params["fs"]), mode='constant', constant_values=0)
     with wave.open("wf.wav", "w") as wav_file: # Dump wav
         wav_file.setnchannels(1)  # Mono
         wav_file.setsampwidth(2)  # 16-bit
@@ -50,6 +54,27 @@ def make_test_wf(m_carriers=m_carriers, m_pulses=m_pulses, p=params): # Create a
     with open("wf_data.txt", "w") as f:
         for idx_bit in range(n_bits):
             f.write(f"{v_bits[idx_bit]}")
+
+    # Plot v_pulse_win
+    plt.figure(figsize=(10, 6))
+    plt.scatter(np.fft.fftfreq(params["n_pulse_len_re"]), 40 * np.log10(v_pulse_win_fft_magnitude))
+    plt.title("Frequency response of Pulse Window applied twice (dB)")
+    plt.xlabel("Frequency Bin")
+    plt.ylabel("Magnitude [dB]")
+    plt.grid()
+    plt.ylim(-40,120)
+    plt.savefig("v_pulse_win_fft.png")
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(np.arange(len(v_pulse_win)) / params["fs"], v_pulse_win)
+    plt.title("Pulse Window Function")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.grid()
+    plt.savefig("v_pulse_win.png")
+    plt.close()
+
 
     # Plot the first 6 pulses
     plt.figure(figsize=(10, 6))
